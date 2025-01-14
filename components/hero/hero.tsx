3a0df95dc8765674c08ff272f7689e55ee0ef7ca
Naz Tekinalp <naz.tekinalp2@hotmail.com>
2025-01-14 16:59:03 +0300

    Add files via upload

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -7,46 +7,45 @@ import MailIcon from "../../assets/icons/mail.svg";
 import MediumIcon from "../../assets/icons/medium.svg";
 import LinkedInIcon from "../../assets/icons/linkedin.svg";
 import ResumeIcon from "../../assets/icons/resume.svg";
-import { Tooltip } from "@nextui-org/react";
 import { useState } from "react";
 import Unicorn from "../unicorn/unicorn";
+import Tooltip from "../tooltip/tooltip";
 
 const Hero = () => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
 
   const tooltipContent = (
-    <div className={style.hero__tooltip_content}>
+    <div>
       {isCopied ? (
         <div>Successfully copied!</div>
       ) : (
-        <div>Click to copy to clipboard</div>
+        <>
+          <div>Click to copy to clipboard</div>
+          <div>tekinalpnaz@gmail.com</div>
+        </>
       )}
-      {!isCopied && <div>tekinalpnaz@gmail.com</div>}
     </div>
   );
 
   return (
     <div className={style.hero__container}>
       <Unicorn />
-      <h1 className={style.hero__title}>
-        Hi, I’m Naz Tekinalp
-      </h1>
+      <h1 className={style.hero__title}>Hi, I'm Naz Tekinalp</h1>
 
       <p className={style.hero__subtitle}>
-        Currently I’m working at EPAM Systems as Software Quality Engineer.
+        Currently I'm working at EPAM Systems as Software Quality Engineer.
       </p>
-             
-      {/*<p className={style.hero__subtitle}>*/}
-      {/*  I&apos;m also a MSc Engineering Management student at IU International University of Applied Sciences.*/}
-      {/*</p>*/}
 
       <div className={style.hero__links}>
-        <Link href="https://github.com/naztekinalpp" passHref>
-          <a target="_blank" rel="noreferrer noopener" title="GitHub">
-            <Image src={GitHubIcon} alt="GitHub" />
-          </a>
+        <Link
+          href="https://github.com/naztekinalpp"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="GitHub"
+        >
+          <Image src={GitHubIcon} alt="GitHub" />
         </Link>
-        <Tooltip content={tooltipContent} title="Personal Mail">
+        <Tooltip content={tooltipContent}>
           <Image
             style={{ cursor: "pointer" }}
             src={MailIcon}
@@ -56,24 +55,25 @@ const Hero = () => {
             onClick={() => {
               navigator.clipboard.writeText("tekinalpnaz@gmail.com");
               setIsCopied(true);
+              setTimeout(() => setIsCopied(false), 2000);
             }}
-            onMouseLeave={() => setTimeout(() => setIsCopied(false), 150)}
           />
         </Tooltip>
-        <Link href="https://medium.com/@naztekinalp" passHref>
-          <a target="_blank" rel="noreferrer noopener" title="Medium">
-            <Image src={MediumIcon} alt="Medium" />
-          </a>
+        <Link
+          href="https://medium.com/@naztekinalp"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Medium"
+        >
+          <Image src={MediumIcon} alt="Medium" />
         </Link>
-        <Link href="https://www.linkedin.com/in/naztekinalp" passHref>
-          <a target="_blank" rel="noreferrer noopener" title="LinkedIn">
-            <Image src={LinkedInIcon} alt="LinkedIn" />
-          </a>
-        </Link>
-        <Link href="/naz-tekinalp.pdf" passHref>
-          <a target="_blank" rel="noreferrer noopener" title="Resume">
-            <Image src={ResumeIcon} alt="Resume" />
-          </a>
+        <Link
+          href="https://www.linkedin.com/in/naztekinalp"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="LinkedIn"
+        >
+          <Image src={LinkedInIcon} alt="LinkedIn" />
         </Link>
       </div>
     </div>
